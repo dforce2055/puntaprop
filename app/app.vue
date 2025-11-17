c457fd05dd2ca672121f88313be25c757f353c70
Diego Pérez <dperez2055@gmail.com>
2025-11-17 13:19:45 -0300

    fix: update SEO metadata and rename project to PuntaProp

--- app/app.vue
+++ app/app.vue
@@ -18,9 +18,9 @@ useHead({
 })
 
 useSeoMeta({
-  titleTemplate: '%s - Nuxt Portfolio Template',
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/portfolio-light.png',
+  titleTemplate: '%s - PuntaProp',
+  ogImage: '/public/hero/punta-1.jpg',
+  twitterImage: '/public/hero/punta-1.jpg',
   twitterCard: 'summary_large_image'
 })
 
